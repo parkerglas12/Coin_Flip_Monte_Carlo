d1357edfe2104791377e2dd17ae4ae454d9fd054
Offbrandcheeto <parcode52@gmail.com>
2025-01-03 10:43:06 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Coin Flip Monte Carlo Simulator
+
+A simple coin flip Monte Carlo simulator written in Python.
+
+## Features
+
+- User can input a number of their choice for the number of simulations they want to perform.
+- Using a NumPy NDarray, the program is extremely fast.
+- A bar chart visualization is provided using Matplotlib and Seaborn.
---
+++ main.py
@@ -0,0 +1,53 @@
+import numpy as np
+from numpy.random import PCG64 as pcg
+from numpy.random import Generator as gen
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def get_user_input() -> int:
+    while True:
+        try:
+            sims: int = int(input("How many coin flips would you like to perform: "))
+            if sims <= 1 or sims > 100000000:
+                raise ValueError("Please enter an integer greater than 1 and less than 100,000,001.")
+        except ValueError as e:
+            print(f"Error: {e}")
+        else:
+            return sims    
+
+def get_results(rng, sims: int) -> tuple[int, int]:
+    results = rng.integers(0, 2, sims)
+    tails: int= np.count_nonzero(results == 0)
+    heads: int = sims - tails
+    return tails, heads
+
+def print_results(tails: int, heads: int, sims: int) -> None:
+    print(f"Of the {sims} flips, tails were flipped {tails} times, and heads were flipped {heads} times.")
+
+def create_bar_chart(tails: int, heads: int, sims: int) -> None:
+    sns.set()
+    sns.set_style("white")
+    plt.figure(figsize=(10, 6))
+    bars = plt.bar(["Tails", "Heads"], [tails, heads], color="#0081cf")
+    plt.xlabel("Outcome", fontsize=14, weight="bold", labelpad=10)
+    plt.xticks(fontsize=12, weight="bold")
+    plt.ylabel("Count",  fontsize=14, weight="bold", labelpad=15)
+    plt.title(f"Monte Carlo Simulation of {sims} Coin Flips",  fontsize=14, weight="bold", pad=15)
+    
+    for bar in bars:
+        yval = bar.get_height()
+        plt.text(bar.get_x() + bar.get_width() / 2, yval, f"{yval:,}", ha="center", va="bottom", fontsize=12, weight="bold")
+
+    sns.despine()
+    plt.tight_layout(pad=1)
+    plt.show()
+
+def main() -> None:
+    rng = gen(pcg()) # Random number generator
+    sims: int = get_user_input()
+    tails, heads = get_results(rng, sims)
+    print_results(tails, heads, sims)
+    create_bar_chart(tails, heads, sims)
+
+if __name__ == "__main__":
+    main()
